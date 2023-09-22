6e6b28810f5c70067ab24916addb1d4e8193ae44
crismouta <crismouta@gmail.com>
2023-09-22 12:06:39 +0200

    hashrouter

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,8 +1,8 @@
 import About from "../components/About";
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import Contact from "../components/Contact";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
     {
         path: "/",
         element: <About />,
